ea62ad6b6ecb3d52f423ebfc4524eb2e6c657b45
mcbyte-it <ramik@inwind.it>
2013-11-30 18:51:18 +0100

    cleanup

--- 1-BOOT_COMPLETED.bat
+++ 1-BOOT_COMPLETED.bat
@@ -2,5 +2,4 @@
 set path=D:\WebDev\AndroidSDK\platform-tools;%PATH%
 
 adb shell am broadcast -a android.intent.action.BOOT_COMPLETED -c android.intent.category.HOME -n com.kattanweb.android.shut/.BootCompleteReceiver
-rem adb shell am broadcast -a android.net.conn.CONNECTIVITY_CHANGE -c android.intent.category.HOME -n android.romstats/.ReportingServiceManager
 pause
